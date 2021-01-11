f5a1f3032af6dfec1e800d9464d1fabe6d8fc0d0
pallaviudatewar <67188959+pallaviudatewar@users.noreply.github.com>
2021-01-12 00:14:23 +0530

    Add files via upload

---
+++ signupphp.php
@@ -0,0 +1,31 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = "miniproject";
+$conn = mysqli_connect($servername, $username, $password, "$dbname");
+if (!$conn) {
+    die('Could not Connect MySql Server:' . mysqli_error($conn));
+}
+
+
+$name = $_POST['name'];
+$contactno = $_POST['contactno'];
+$emailid = $_POST['emailid'];
+$passw = $_POST['passw'];
+
+$sql = "INSERT INTO maintable (name,contact_no,email_id,password)
+     VALUES ('$name','$contactno','$emailid','$passw')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "<script>
+        alert('Account Generated. Please LogIn.');
+        location = 'Login.html';
+        </script>";
+    //echo "New record has been added successfully !";
+    // header("Location: login.html");
+    exit();
+} else {
+    echo "Error: " . $sql . ":-" . mysqli_error($conn);
+}
+mysqli_close($conn);
