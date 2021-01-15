d7c0008174365d7e0d28a1bf2884b725d9134c5d
7lakshanya <68146973+7lakshanya@users.noreply.github.com>
2021-01-15 22:00:52 +0530

    add sell pages

---
+++ sell/sell.js
@@ -0,0 +1,8 @@
+function sell(){
+  document.getElementById("section-2").classList.remove("invisible");
+    document.getElementById("section-3").classList.add("invisible");
+}
+function rentOut(){
+  document.getElementById("section-2").classList.add("invisible");
+  document.getElementById("section-3").classList.remove("invisible");
+}
