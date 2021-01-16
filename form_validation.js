ccd01e8d665d9abe1e649bf69cbf9aecd4c44a1c
7lakshanya <68146973+7lakshanya@users.noreply.github.com>
2021-01-17 00:59:29 +0530

    changed sellrent

---
+++ form_validation.js
@@ -0,0 +1,9 @@
+function sell() {
+  document.getElementById("section-2").classList.remove("invisible");
+  document.getElementById("section-3").classList.add("invisible");
+}
+
+function rent() {
+  document.getElementById("section-3").classList.remove("invisible");
+  document.getElementById("section-2").classList.add("invisible");
+}
