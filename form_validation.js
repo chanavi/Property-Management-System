ceb3f4c032ee424cfeed3069dbe5fb01b02ef317
chanavi <57930423+chanavi@users.noreply.github.com>
2021-01-31 12:37:18 +0530

    Delete form_validation.js

--- form_validation.js
+++
@@ -1,9 +0,0 @@
-function sell() {
-  document.getElementById("section-2").classList.remove("invisible");
-  document.getElementById("section-3").classList.add("invisible");
-}
-
-function rent() {
-  document.getElementById("section-3").classList.remove("invisible");
-  document.getElementById("section-2").classList.add("invisible");
-}
